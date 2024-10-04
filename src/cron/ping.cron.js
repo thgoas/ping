698477359f87fc7e03ad00a669e5cb8036d8b6b7
thgoas <thgas95@gmail.com>
2024-10-04 15:14:55 -0300

    refactor: change local function enviar alerta

--- src/cron/ping.cron.js
+++ src/cron/ping.cron.js
@@ -1,58 +1,15 @@
 
 const cron = require('node-cron');
-const { findAll } = require('../models/loja.model');
-const alertaModel = require('../models/alerta.model');
-const { pingIp }= require('../service/ping');
+const minutoModel = require('../models/minuto.model');
+const {enviarAlerta} = require('../service/enviar.alerta');
+
+
 function cronJob() {
     cron.schedule('* * * * *', async function () {
         try {
-
-            const result = await findAll();
-
-            result.forEach(async (loja) => {
-                if(loja.status === 0){
-                    return;
-                }
-                const result = await pingIp(loja.ip);
-                if (!result) {
-                    const alerta = await alertaModel.findById(loja.nome);
-                    console.log(alerta)
-                    if (!alerta?.nome) {
-
-                        await alertaModel.create({
-                            nome: loja.nome,
-                            ip: loja.ip,
-                            data: new Date().getTime()
-
-                        })
-                        await fetch(`${process.env.BASE_URL}/alert/${loja.tipo}`, {
-                            method: 'post',
-                            body: JSON.stringify(loja),
-                            headers: {
-                                'Content-Type': 'application/json'
-                            }
-                        })
-                    } else {
-                        if (alerta.data + (5 * 60 * 1000)  <= new Date().getTime()) {
-                            await alertaModel.delete(loja.nome);
-                            await alertaModel.create({
-                                nome: loja.nome,
-                                ip: loja.ip,
-                                data: new Date().getTime()
-
-                            })
-
-                           await fetch(`${process.env.BASE_URL}/alert/${loja.tipo}`, {
-                                method: 'post',
-                                body: JSON.stringify(loja),
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                }
-                            })
-                        }
-                    }
-                }
-            });
+            const minuto = await minutoModel.findAll();
+            await enviarAlerta(minuto[0].valor);
+          
         } catch (error) {
             console.log(error);
         }
---
+++ src/service/enviar.alerta.js
@@ -0,0 +1,66 @@
+const lojaModel = require('../models/loja.model');
+const alertaModel = require('../models/alerta.model');
+const { pingIp }= require('./ping');
+
+async function enviarAlerta(minutos = 10) {
+    try {
+
+        const lojas = await lojaModel.findAll();
+
+        lojas.forEach(async (loja) => {
+            if(loja.status === 0){
+                return;
+            }
+            const ping = await pingIp(loja.ip);
+            if (!ping) {
+                const alerta = await alertaModel.findById(loja.nome);
+                console.log(alerta)
+                if (!alerta?.nome) {
+                    console.log('Alerta não encontrado');
+                    await alertaModel.create({
+                        nome: loja.nome,
+                        ip: loja.ip,
+                        data: new Date().getTime() + (minutos * 60 * 1000)
+
+                    })
+                    await fetch(`${process.env.BASE_URL}/alert/${loja.tipo}`, {
+                        method: 'post',
+                        body: JSON.stringify(loja),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                } else {
+                    if (Number(alerta.data)  <= new Date().getTime()) {
+                        console.log('Alerta expirado');
+                        await alertaModel.delete(loja.nome);
+                        await alertaModel.create({
+                            nome: loja.nome,
+                            ip: loja.ip,
+                            data: new Date().getTime()  + (5 * 60 * 1000)
+
+                        })
+
+                       await fetch(`${process.env.BASE_URL}/alert/${loja.tipo}`, {
+                            method: 'post',
+                            body: JSON.stringify(loja),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                    }
+                }
+            } else {
+                const alerta = await alertaModel.findById(loja.nome);
+
+                if (alerta?.nome) {
+                    await alertaModel.delete(loja.nome);
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {enviarAlerta}
